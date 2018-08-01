408b3d1ffadfc4ec1d7fd197e65d5f91ea79dae4
Piotr Russ <mail@pruss.it>
2018-08-01 19:56:20 +0200

    fine-tuning

--- keymap.c
+++ keymap.c
@@ -41,38 +41,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
- * | Stop |      |      |  (   |   )  |      |      |      |      |      |      | INSRT|
+ * | Stop | RGB_T| RGB_M|  (   |   )  |      |      |      |      |      |      | INSRT|
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Play | Vol+ | Next |  [   |   ]  |      | Left | Down |  Up  |Right | PGUP | HOME |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift| Vol- | Prev |  {   |   }  |      |   0  |   ^  |   %  |   $  | PGDN | END  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Mute | Alt  | GUI  |Lower | RGB_T| RGB_M|Raise | LED  |LED_i |LED_d |PRNSCR|
+ * | Ctrl | Mute | Alt  | GUI  |Lower |      |      |Raise | LED  |LED_i |LED_d |PRNSCR|
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_ortho_4x12( \
-  KC_MSTP, _______,  _______,  KC_LPRN,  KC_RPRN,  _______,  _______,  _______,  _______,  _______,  _______,  KC_INS, \
+  KC_MSTP, RGB_TOG,  RGB_MOD,  KC_LPRN,  KC_RPRN,  _______,  _______,  _______,  _______,  _______,  _______,  KC_INS, \
   KC_MPLY, KC_VOLU,  KC_MNXT,  KC_LBRC,  KC_RBRC,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_PGUP,  KC_HOME, \
   KC_LSFT, KC_VOLD,  KC_MPRV,  KC_LCBR,  KC_RCBR,  _______,  KC_0,     KC_CIRC,  KC_PERC,  KC_DLR,   KC_PGDN,  KC_END, \
-  KC_LCTL, KC_MUTE,  KC_LALT,  KC_LGUI,  _______,  RGB_TOG,  RGB_MOD,  _______,  BL_TOGG,  BL_INC,   BL_DEC,   KC_PSCR \
+  KC_LCTL, KC_MUTE,  KC_LALT,  KC_LGUI,  _______,  _______,  _______,  _______,  BL_TOGG,  BL_INC,   BL_DEC,   KC_PSCR \
 ),
 
 /* Rise
  * ,-----------------------------------------------------------------------------------.
- * |   ~  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |   =  |
+ * |  F12 |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |   +  |
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |   =  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Shift|   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |   -  |
+ * | Shift|   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |   _  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |      | Alt  | GUI  |Lower |      |      |Raise |      |      |      |   _  |  
+ * | Ctrl |      | Alt  | GUI  |Lower |      |      |Raise |      |      |      |      |  
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_ortho_4x12( \
-  S(KC_GRAVE), KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_EQL, \
-  KC_GRAVE,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PLUS, \
-  KC_LSFT,    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_MINS, \
-  KC_LCTL,    _______, KC_LALT, KC_LGUI, _______, _______, _______, _______, _______, _______, _______, KC_UNDS  \
+  KC_F12,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_EQL, \
+  KC_GRAVE, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL, \
+  KC_LSFT,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, \
+  KC_LCTL,  _______, KC_LALT, KC_LGUI, _______, _______, _______, _______, _______, _______, _______, _______ \
 )
 };
 
